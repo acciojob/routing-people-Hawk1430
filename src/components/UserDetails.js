cacd4f1e06e6136b87442ab70e313a8addcbe8d4
Prashant Raj <pr2125116@gmail.com>
2025-06-20 18:29:00 +0530

    Done

--- src/components/UserDetails.js
+++ src/components/UserDetails.js
@@ -5,22 +5,20 @@ const UserDetails = () => {
     const {id} = useParams();
     const [user, setUser] = useState(null);
     const [loading, setLoading] = useState(true);
-    useEffect(()=>{
-        fetch(`https://jsonplaceholder.typicode.com/users/${id}`)
-            .then(res => res.json())
-            .then(data => {
-                setUser(data);
-                setLoading(false);
-                console.log(user)
-            })
-            .catch(err => {
-                console.error('Error: ',err);
-                setLoading(false);
-            })
-    },[id])
+  useEffect(() => {
+    setLoading(true);
+    fetch(`https://jsonplaceholder.typicode.com/users/${id}`)
+      .then((res) => res.json())
+      .then((data) => {
+        setTimeout(() => {
+          setUser(data);
+          setLoading(false);
+        }, 1000);
+      });
+  }, [id]);
 
     if(loading) return <div>Loading...</div>
-    if(!user) return <p>User not found.</p>
+
   return (
     <>
         <h1>User Details</h1>
@@ -29,6 +27,7 @@ const UserDetails = () => {
         <p>{`Email: ${user.email}`}</p>
         <p>{`Phone: ${user.phone}`}</p>
         <p>{`Website: ${user.website}`}</p>
+        <Link to="/">Back to User List</Link>
     </>
   )
 }
