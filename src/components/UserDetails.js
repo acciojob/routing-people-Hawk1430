b79ad18ee2d254d5e50ea565133648bff0173de1
Prashant Raj <pr2125116@gmail.com>
2025-06-20 18:30:51 +0530

    Done

--- src/components/UserDetails.js
+++ src/components/UserDetails.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react'
+import { Link } from 'react-router-dom/cjs/react-router-dom';
 import { useParams } from 'react-router-dom/cjs/react-router-dom.min'
 
 const UserDetails = () => {
